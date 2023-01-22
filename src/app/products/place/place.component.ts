aa10135e29a281420029bb65c917ae8c859ac6aa
Sreehari <sreehari26012000.com>
2023-01-22 23:00:45 +0530

    update payment

--- src/app/products/place/place.component.ts
+++ src/app/products/place/place.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component,OnInit,HostListener } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-place',
@@ -15,15 +16,18 @@ export class PlaceComponent implements OnInit {
  confirmForm = this.fb.group({
   firstName:['',[Validators.required,Validators.pattern("[a-zA-Z0-9]*")]],
   lastName:['',[Validators.required,Validators.pattern("[a-zA-Z0-9]*")]],
-  addressLine1:['',[Validators.required,Validators.pattern("[a-zA-Z0-9]*")]],
-  addressLine2:['',[Validators.required,Validators.pattern("[a-zA-Z0-9]*")]],
+  addressLine1:['',[Validators.required,Validators.pattern("[a-zA-Z]*")]],
+  addressLine2:['',[Validators.required,Validators.pattern("[a-zA-Z]*")]],
   phone:['',[Validators.required,Validators.pattern("[0-9]*")]],
   email:['',[Validators.required,Validators.pattern("[a-zA-Z0-9]*")]]
  })
-
-  constructor  (private fb :FormBuilder,private router:Router,private http :HttpClient)  { }
-   ngOnInit(): void {
-     
+ 
+  constructor  (private fb :FormBuilder,private router:Router,private http :HttpClient,private cart:CartService)  { }
+  GrandTotal:number=0
+  strikeCheckout:any = null; 
+  ngOnInit(): void {
+    this.stripePaymentGateway();
+    this.GrandTotal = this.cart.getTotal()
    }
 
    confirm() {
@@ -47,4 +51,44 @@ export class PlaceComponent implements OnInit {
     console.log(firstName);
    
    }
+   checkout(amount:any) {
+    const strikeCheckout = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_51ML3kcSCIwfCBTR1F6SHCfwRE4nT2slZNjNhyMtBL4S5h0sNIsGMPFM2Hw72GDYVZHtpO9gxlhjnR4qrA21k9aLE00n9cOGScA',
+      locale: 'auto',
+      token: function (stripeToken: any) {
+        console.log(stripeToken)
+        alert('Payment Successfull!');
+        
+
+      }
+    });
+  
+    strikeCheckout.open({
+      name: 'RemoteStack',
+      description: 'Payment widgets',
+      amount: this.GrandTotal
+    });
+  }
+  
+  stripePaymentGateway() {
+    if(!window.document.getElementById('stripe-script')) {
+      const scr = window.document.createElement("script");
+      scr.id = "stripe-script";
+      scr.type = "text/javascript";
+      scr.src = "https://checkout.stripe.com/checkout.js";
+
+      scr.onload = () => {
+        this.strikeCheckout = (<any>window).StripeCheckout.configure({
+          key: 'pk_test_12239293949ksdfksdjkfj1232q3jkjssdfjk',
+          locale: 'auto',
+          token: function (token: any) {
+            console.log(token)
+            alert('Payment via stripe successfull!');
+          }
+        });
+      }
+        
+      window.document.body.appendChild(scr);
+    }
+  }
 }
